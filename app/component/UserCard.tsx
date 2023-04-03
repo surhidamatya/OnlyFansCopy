0ab91afbc37408e2f43e26f84055d9f908fb869d
Surhid Amatya <surhid.amatya@gmail.com>
2023-04-03 15:25:40 +0545

    separate user card component

---
+++ app/component/UserCard.tsx
@@ -0,0 +1,47 @@
+import { Image, ImageBackground, StyleSheet, Text, View } from "react-native";
+import React from 'react';
+
+const UserCard = (props) => {
+    const user = props.user;
+  return (
+    <ImageBackground source= {{uri: user.coverImage}} style={styles.userCard}>
+        <View style = {styles.overlay}/>
+        <Image src = {user.avatar} 
+        style = {styles.userImage}/>
+        <View>
+        <Text style= {{color: 'white', fontSize: 22, fontWeight: '500'}}>{user.name}</Text>
+        <Text style= {{color: 'white'}}>@{user.handle}</Text>
+        </View>
+
+      </ImageBackground>
+  )
+}
+
+export default UserCard;
+const styles = StyleSheet.create({
+    userCard:{
+      backgroundColor: 'gray',
+      padding: 10,
+      flexDirection:'row',
+      alignItems: 'flex-end',
+      borderRadius: 10,
+      overflow: 'hidden',
+      marginVertical: 5
+    },
+    overlay:{
+      backgroundColor: 'rgba(0,0,0,0.3)',
+      position: 'absolute',
+      top:0,
+      bottom:0,
+      left:0,
+      right:0
+    },
+    userImage:{
+      height: 100,
+      width: 100,
+      borderRadius: 100,
+      borderColor: 'white',
+      borderWidth: 5,
+      marginRight: 20
+    }
+  });
--- app/index.js
+++ app/index.js
@@ -1,21 +1,16 @@
 import { Image, ImageBackground, StyleSheet, Text, View } from "react-native";
-import users from '../assets/data/users';
+import UserCard from "./component/UserCard";
 
+
+
+import users from '../assets/data/users';
 const user = users[0];
 
 export default function Page() {
   return (
     <View style={styles.container}>
-      <ImageBackground source= {{uri: user.coverImage}} style={styles.userCard}>
-        <View style = {styles.overlay}/>
-        <Image src = {user.avatar} 
-        style = {styles.userImage}/>
-        <View>
-        <Text style= {{color: 'white', fontSize: 22, fontWeight: '500'}}>{user.name}</Text>
-        <Text style= {{color: 'white'}}>@{user.handle}</Text>
-        </View>
-
-      </ImageBackground>
+      <UserCard user = {users[0]}/>
+      <UserCard user = {users[1]}/>
     </View>
   );
 }
@@ -25,29 +20,5 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 12,
     paddingTop: 75
-  },
-  userCard:{
-    backgroundColor: 'gray',
-    padding: 10,
-    flexDirection:'row',
-    alignItems: 'flex-end',
-    borderRadius: 10,
-    overflow: 'hidden'
-  },
-  overlay:{
-    backgroundColor: 'rgba(0,0,0,0.3)',
-    position: 'absolute',
-    top:0,
-    bottom:0,
-    left:0,
-    right:0
-  },
-  userImage:{
-    height: 100,
-    width: 100,
-    borderRadius: 100,
-    borderColor: 'white',
-    borderWidth: 5,
-    marginRight: 20
   }
 });
